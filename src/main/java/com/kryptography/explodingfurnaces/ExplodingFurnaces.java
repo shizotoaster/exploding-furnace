810d277857b13ea6adce90462e91a1812507882d
Kryptography <114697909+krypt0graphy@users.noreply.github.com>
2023-07-06 22:16:03 +0300

    You wouldn't put gunpowder in a furnace.

--- src/main/java/com/kryptography/explodingfurnaces/ExplodingFurnaces.java
+++ src/main/java/com/kryptography/explodingfurnaces/ExplodingFurnaces.java
@@ -2,10 +2,6 @@ package com.kryptography.explodingfurnaces;
 
 
 import com.kryptography.explodingfurnaces.registry.ItemInit;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
